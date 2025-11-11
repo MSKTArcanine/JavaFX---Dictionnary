9666214d6821f26ed2a48f71887715f3dad4dc6e
MSKTArcanine <marcantoine1999@live.fr>
2025-11-11 19:21:52 +0100

    gets

--- src/main/java/fr/awu/dictionnary/Dictionnary.java
+++ src/main/java/fr/awu/dictionnary/Dictionnary.java
@@ -2,21 +2,29 @@ package fr.awu.dictionnary;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 public class Dictionnary {
-    private HashMap<String, ArrayList<String>> dictionnary;
+    private HashMap<String, String> dictionnary;
+    private ArrayList<String> words;
+    private Random random = new Random();
 
     public Dictionnary() {
         this.dictionnary = new HashMap<>();
     }
 
     public void add(String w, String t){
-        if(!this.dictionnary.containsKey(w)){
-            this.dictionnary.put(w, new ArrayList<String>());
-        }
-        if(!this.dictionnary.get(w).contains(t)){
-            this.dictionnary.get(w).add(t);
-        }
+        if(!this.words.contains(w)) this.words.add(w);
+        this.dictionnary.put(w, t);
+    }
+
+    public String get(String w) throws Exception{
+        if(!this.dictionnary.containsKey(w)) throw new IllegalArgumentException("Word doesn't exist");
+        return this.dictionnary.get(w);
+    }
+
+    public String getRandom(){
+        return this.words.get(random.nextInt(this.words.size()));
     }
     
 }
